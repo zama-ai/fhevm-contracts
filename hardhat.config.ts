512b7972f5cfc5a9ae02bbb7ca3bfe7ba3cf16c3
Clément 'birdy' Danjou <clement@danjou.io>
2023-12-12 12:21:12 +0100

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,94 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import "hardhat-deploy";
+import type { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
+
+import "./tasks/accounts";
+import "./tasks/deployERC20";
+import "./tasks/getEthereumAddress";
+import "./tasks/mint";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const chainIds = {
+  zama: 8009,
+  local: 9000,
+};
+
+function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
+  let jsonRpcUrl: string;
+  switch (chain) {
+    case "local":
+      jsonRpcUrl = "http://localhost:8545";
+      break;
+    case "zama":
+      jsonRpcUrl = "https://devnet.zama.ai";
+      break;
+  }
+  return {
+    accounts: {
+      count: 10,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[chain],
+    url: jsonRpcUrl,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "local",
+  namedAccounts: {
+    deployer: 0,
+  },
+  mocha: {
+    timeout: 180000,
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    zama: getChainConfig("zama"),
+    local: getChainConfig("local"),
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      metadata: {
+        // Not including the metadata hash
+        // https://github.com/paulrberg/hardhat-template/issues/31
+        bytecodeHash: "none",
+      },
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
