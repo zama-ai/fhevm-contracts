5a65a2a13cecaec9e9986f3a249d75b0835cb0fc
Joseph-André Turk <josephandre.turk@zama.ai>
2024-03-06 13:38:43 +0100

    feat: updated ethermint and added CI
    fix: typo workflows

--- test/types.ts
+++ test/types.ts
@@ -1,17 +1,5 @@
 import type { FhevmInstance } from "fhevmjs";
 
-import { EncryptedERC20 } from "../types";
-import type { Signers } from "./signers";
-
-declare module "mocha" {
-  export interface Context {
-    signers: Signers;
-    contractAddress: string;
-    instances: FhevmInstances;
-    erc20: EncryptedERC20;
-  }
-}
-
 export interface FhevmInstances {
   alice: FhevmInstance;
   bob: FhevmInstance;
