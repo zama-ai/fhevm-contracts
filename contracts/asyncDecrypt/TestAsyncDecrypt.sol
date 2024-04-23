3e6a797a36be1d24284f7d6f3fcebd7713e08bf7
Joseph-André Turk <josephandre.turk@zama.ai>
2024-04-23 17:29:00 +0200

    chore: updated to fhevm 0.4.0
    chore: prettier

---
+++ contracts/asyncDecrypt/TestAsyncDecrypt.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity ^0.8.20;
+
+import "fhevm/lib/TFHE.sol";
+import "fhevm/oracle/OracleCaller.sol";
+
+contract TestAsyncDecrypt is OracleCaller {
+    ebool xBool;
+    euint4 xUint4;
+    euint8 xUint8;
+    euint16 xUint16;
+    euint32 xUint32;
+    euint64 xUint64;
+    eaddress xAddress;
+
+    bool public yBool;
+    uint8 public yUint4;
+    uint8 public yUint8;
+    uint16 public yUint16;
+    uint32 public yUint32;
+    uint64 public yUint64;
+    address public yAddress;
+
+    constructor() {
+        xBool = TFHE.asEbool(true);
+        xUint4 = TFHE.asEuint4(4);
+        xUint8 = TFHE.asEuint8(42);
+        xUint16 = TFHE.asEuint16(16);
+        xUint32 = TFHE.asEuint32(32);
+        xUint64 = TFHE.asEuint64(64);
+        xAddress = TFHE.asEaddress(0x8ba1f109551bD432803012645Ac136ddd64DBA72);
+    }
+
+    function requestBoolAboveDelay() public {
+        // should revert
+        ebool[] memory cts = new ebool[](1);
+        cts[0] = xBool;
+        Oracle.requestDecryption(cts, this.callbackBool.selector, 0, block.timestamp + 2 days);
+    }
+
+    function requestBool() public {
+        ebool[] memory cts = new ebool[](1);
+        cts[0] = xBool;
+        Oracle.requestDecryption(cts, this.callbackBool.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeBool() public {
+        ebool[] memory cts = new ebool[](1);
+        cts[0] = ebool.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackBool.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackBool(uint256 /*requestID*/, bool decryptedInput) public onlyOracle returns (bool) {
+        yBool = decryptedInput;
+        return yBool;
+    }
+
+    function requestUint4() public {
+        euint4[] memory cts = new euint4[](1);
+        cts[0] = xUint4;
+        Oracle.requestDecryption(cts, this.callbackUint4.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeUint4() public {
+        euint4[] memory cts = new euint4[](1);
+        cts[0] = euint4.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackUint4.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackUint4(uint256 /*requestID*/, uint8 decryptedInput) public onlyOracle returns (uint8) {
+        yUint4 = decryptedInput;
+        return decryptedInput;
+    }
+
+    function requestUint8() public {
+        euint8[] memory cts = new euint8[](1);
+        cts[0] = xUint8;
+        Oracle.requestDecryption(cts, this.callbackUint8.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeUint8() public {
+        euint8[] memory cts = new euint8[](1);
+        cts[0] = euint8.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackUint8.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackUint8(uint256 /*requestID*/, uint8 decryptedInput) public onlyOracle returns (uint8) {
+        yUint8 = decryptedInput;
+        return decryptedInput;
+    }
+
+    function requestUint16() public {
+        euint16[] memory cts = new euint16[](1);
+        cts[0] = xUint16;
+        Oracle.requestDecryption(cts, this.callbackUint16.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeUint16() public {
+        euint16[] memory cts = new euint16[](1);
+        cts[0] = euint16.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackUint16.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackUint16(uint256 /*requestID*/, uint16 decryptedInput) public onlyOracle returns (uint16) {
+        yUint16 = decryptedInput;
+        return decryptedInput;
+    }
+
+    function requestUint32(uint32 input1, uint32 input2) public {
+        euint32[] memory cts = new euint32[](1);
+        cts[0] = xUint32;
+        uint256 requestID = Oracle.requestDecryption(cts, this.callbackUint32.selector, 0, block.timestamp + 100);
+        addParamsUint(requestID, input1);
+        addParamsUint(requestID, input2);
+    }
+
+    function requestFakeUint32() public {
+        euint32[] memory cts = new euint32[](1);
+        cts[0] = euint32.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackUint32.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackUint32(uint256 requestID, uint32 decryptedInput) public onlyOracle returns (uint32) {
+        uint256[] memory params = getParamsUint(requestID);
+        unchecked {
+            uint32 result = uint32(params[0]) + uint32(params[1]) + decryptedInput;
+            yUint32 = result;
+            return result;
+        }
+    }
+
+    function requestUint64() public {
+        euint64[] memory cts = new euint64[](1);
+        cts[0] = xUint64;
+        Oracle.requestDecryption(cts, this.callbackUint64.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeUint64() public {
+        euint64[] memory cts = new euint64[](1);
+        cts[0] = euint64.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackUint64.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackUint64(uint256 /*requestID*/, uint64 decryptedInput) public onlyOracle returns (uint64) {
+        yUint64 = decryptedInput;
+        return decryptedInput;
+    }
+
+    function requestAddress() public {
+        eaddress[] memory cts = new eaddress[](1);
+        cts[0] = xAddress;
+        Oracle.requestDecryption(cts, this.callbackAddress.selector, 0, block.timestamp + 100);
+    }
+
+    function requestFakeAddress() public {
+        eaddress[] memory cts = new eaddress[](1);
+        cts[0] = eaddress.wrap(42);
+        Oracle.requestDecryption(cts, this.callbackAddress.selector, 0, block.timestamp + 100); // this should revert because previous ebool is not honestly obtained
+    }
+
+    function callbackAddress(uint256 /*requestID*/, address decryptedInput) public onlyOracle returns (address) {
+        yAddress = decryptedInput;
+        return decryptedInput;
+    }
+}
---
+++ test/oracleDecrypt/testAsyncDecrypt.ts
@@ -0,0 +1,234 @@
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+
+import { asyncDecrypt, awaitAllDecryptionResults } from "../asyncDecrypt";
+import { getSigners, initSigners } from "../signers";
+
+describe("TestAsyncDecrypt", function () {
+  before(async function () {
+    await asyncDecrypt();
+    await initSigners(3);
+    this.signers = await getSigners();
+  });
+
+  beforeEach(async function () {
+    const contractFactory = await ethers.getContractFactory("TestAsyncDecrypt");
+    this.contract = await contractFactory.connect(this.signers.alice).deploy();
+  });
+
+  it("test async decrypt bool would fail if maxTimestamp is above 1 day", async function () {
+    if (network.name === "hardhat") {
+      // mocked mode
+      await expect(this.contract.connect(this.signers.carol).requestBoolAboveDelay()).to.be.revertedWith(
+        "maxTimestamp exceeded MAX_DELAY",
+      );
+    } else {
+      // fhevm-mode
+      const tx = await this.contract.connect(this.signers.carol).requestBoolAboveDelay({ gasLimit: 1_000_000 });
+      await expect(tx.wait()).to.throw;
+    }
+  });
+
+  it("test async decrypt bool", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestBool({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yBool();
+    expect(y).to.equal(true);
+  });
+
+  it("test async decrypt FAKE bool", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeBool.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt uint4", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestUint4({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yUint4();
+    expect(y).to.equal(4);
+  });
+
+  it("test async decrypt FAKE uint4", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeUint4.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt uint8", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestUint8({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yUint8();
+    expect(y).to.equal(42);
+  });
+
+  it("test async decrypt FAKE uint8", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeUint8.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt uint16", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestUint16({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yUint16();
+    expect(y).to.equal(16);
+  });
+
+  it("test async decrypt FAKE uint16", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeUint16.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt uint32", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestUint32(5, 15, { gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yUint32();
+    expect(y).to.equal(52); // 5+15+32
+  });
+
+  it("test async decrypt FAKE uint32", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeUint32.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt uint64", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestUint64({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yUint64();
+    expect(y).to.equal(64);
+  });
+
+  it("test async decrypt FAKE uint64", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeUint64.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+
+  it("test async decrypt address", async function () {
+    const tx2 = await this.contract.connect(this.signers.carol).requestAddress({ gasLimit: 500_000 });
+    await tx2.wait();
+    await awaitAllDecryptionResults();
+    const y = await this.contract.yAddress();
+    expect(y).to.equal("0x8ba1f109551bD432803012645Ac136ddd64DBA72");
+  });
+
+  it("test async decrypt FAKE address", async function () {
+    if (network.name !== "hardhat") {
+      // only in fhevm mode
+      const txObject = await this.contract
+        .connect(this.signers.carol)
+        .requestFakeAddress.populateTransaction({ gasLimit: 5_000_000 });
+      const tx = await this.signers.carol.sendTransaction(txObject);
+      let receipt = null;
+      let waitTime = 0;
+      while (receipt === null && waitTime < 15000) {
+        receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        if (receipt === null) {
+          console.log("Trying again to fetch txn receipt....");
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds
+          waitTime += 5000;
+        }
+      }
+      expect(waitTime >= 15000).to.be.true;
+    }
+  });
+});
