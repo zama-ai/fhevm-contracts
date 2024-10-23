0c9621eb593a33744076d6bee63f4e9367fc1d73
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-10-23 15:10:16 +0200

    refactor: Test helpers for v0.5

--- test/instance.ts
+++ test/instance.ts
@@ -1,87 +1,202 @@
-import { toBufferBE } from "bigint-buffer";
-import { Signer } from "ethers";
-import fhevmjs, { FhevmInstance, getPublicKeyCallParams } from "fhevmjs";
-import { ethers as hethers } from "hardhat";
+import dotenv from "dotenv";
+import { clientKeyDecryptor, createInstance as createFhevmInstance, getCiphertextCallParams } from "fhevmjs";
+import { readFileSync } from "fs";
+import * as fs from "fs";
+import { ethers, ethers as hethers, network } from "hardhat";
+import { homedir } from "os";
+import path from "path";
 
+import { awaitCoprocessor, getClearText } from "./coprocessorUtils";
+import { createEncryptedInputMocked, reencryptRequestMocked } from "./fhevmjsMocked";
 import type { Signers } from "./signers";
 import { FhevmInstances } from "./types";
 
-const HARDHAT_NETWORK = process.env.HARDHAT_NETWORK;
+const FHE_CLIENT_KEY_PATH = process.env.FHE_CLIENT_KEY_PATH;
 
-let publicKey: string | undefined;
-let chainId: number;
+let clientKey: Uint8Array | undefined;
 
-export const createInstances = async (
-  contractAddress: string,
-  ethers: typeof hethers,
-  accounts: Signers,
-): Promise<FhevmInstances> => {
-  // Create instance
-  const instances: FhevmInstances = {} as FhevmInstances;
-  await Promise.all(
-    Object.keys(accounts).map(async (k) => {
-      instances[k as keyof FhevmInstances] = await createInstance(
-        contractAddress,
-        accounts[k as keyof Signers],
-        ethers,
-      );
-    }),
-  );
+const parsedEnvACL = dotenv.parse(fs.readFileSync("node_modules/fhevm/lib/.env.acl"));
+const aclAdd = parsedEnvACL.ACL_CONTRACT_ADDRESS;
 
-  return instances;
-};
-
-export const createInstance = async (contractAddress: string, account: Signer, ethers: typeof hethers) => {
-  // 1. Get chain id
-  const provider = ethers.provider;
-
-  const network = await provider.getNetwork();
-  chainId = +network.chainId.toString(); // Need to be a number
-  try {
-    // Get blockchain public key
-    const ret = await provider.call(getPublicKeyCallParams());
-    const decoded = ethers.AbiCoder.defaultAbiCoder().decode(["bytes"], ret);
-    publicKey = decoded[0];
-  } catch (e) {
-    publicKey = undefined;
-  }
-
-  const instance = await fhevmjs.createInstance({ chainId, publicKey });
-
-  if (HARDHAT_NETWORK === "hardhat") {
-    instance.encryptBool = createUintToUint8ArrayFunction(1);
-    instance.encrypt4 = createUintToUint8ArrayFunction(4);
-    instance.encrypt8 = createUintToUint8ArrayFunction(8);
-    instance.encrypt16 = createUintToUint8ArrayFunction(16);
-    instance.encrypt32 = createUintToUint8ArrayFunction(32);
-    instance.encrypt64 = createUintToUint8ArrayFunction(64);
-    instance.encryptAddress = createUintToUint8ArrayFunction(160);
-    instance.decrypt = (_, hexadecimalString) => BigInt(hexadecimalString);
-    instance.decryptAddress = (_, hexadecimalString) => ethers.getAddress(hexadecimalString.slice(26, 66));
-  }
-  await generatePublicKey(contractAddress, account, instance);
-
+const createInstanceMocked = async () => {
+  const instance = await createFhevmInstance({
+    chainId: network.config.chainId,
+  });
+  instance.reencrypt = reencryptRequestMocked;
+  instance.createEncryptedInput = createEncryptedInputMocked;
+  instance.getPublicKey = () => "0xFFAA44433";
   return instance;
 };
 
-const generatePublicKey = async (contractAddress: string, signer: Signer, instance: FhevmInstance) => {
-  // Generate token to decrypt
-  const generatedToken = instance.generatePublicKey({
-    verifyingContract: contractAddress,
-  });
-  // Sign the public key
-  const signature = await signer.signTypedData(
-    generatedToken.eip712.domain,
-    { Reencrypt: generatedToken.eip712.types.Reencrypt }, // Need to remove EIP712Domain from types
-    generatedToken.eip712.message,
-  );
-  instance.setSignature(contractAddress, signature);
+export const createInstances = async (accounts: Signers): Promise<FhevmInstances> => {
+  // Create instance
+  const instances: FhevmInstances = {} as FhevmInstances;
+  if (network.name === "hardhat") {
+    await Promise.all(
+      Object.keys(accounts).map(async (k) => {
+        instances[k as keyof FhevmInstances] = await createInstanceMocked();
+      }),
+    );
+  } else {
+    await Promise.all(
+      Object.keys(accounts).map(async (k) => {
+        instances[k as keyof FhevmInstances] = await createInstance();
+      }),
+    );
+  }
+  return instances;
 };
 
-function createUintToUint8ArrayFunction(numBits: number) {
-  const numBytes = Math.ceil(numBits / 8);
-  return function (uint: number | bigint | boolean) {
-    const buffer = toBufferBE(BigInt(uint), numBytes);
-    return buffer;
-  };
-}
+export const createInstance = async () => {
+  const instance = await createFhevmInstance({
+    networkUrl: network.config.url,
+    gatewayUrl: "http://localhost:7077",
+    aclAddress: aclAdd,
+  });
+  return instance;
+};
+
+const getCiphertext = async (handle: bigint, ethers: typeof hethers): Promise<string> => {
+  return ethers.provider.call(getCiphertextCallParams(handle));
+};
+
+const getDecryptor = () => {
+  if (clientKey == null) {
+    if (FHE_CLIENT_KEY_PATH) {
+      clientKey = readFileSync(FHE_CLIENT_KEY_PATH);
+    } else {
+      const home = homedir();
+      const clientKeyPath = path.join(home, "network-fhe-keys/cks");
+      clientKey = readFileSync(clientKeyPath);
+    }
+  }
+  return clientKeyDecryptor(clientKey);
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {bool}
+ */
+export const decryptBool = async (handle: bigint): Promise<boolean> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return (await getClearText(handle)) === "1";
+  } else {
+    return getDecryptor().decryptBool(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} handle to decrypt
+ * @returns {bigint}
+ */
+export const decrypt4 = async (handle: bigint): Promise<bigint> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return BigInt(await getClearText(handle));
+  } else {
+    return getDecryptor().decrypt4(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {bigint}
+ */
+export const decrypt8 = async (handle: bigint): Promise<bigint> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return BigInt(await getClearText(handle));
+  } else {
+    return getDecryptor().decrypt8(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {bigint}
+ */
+export const decrypt16 = async (handle: bigint): Promise<bigint> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return BigInt(await getClearText(handle));
+  } else {
+    return getDecryptor().decrypt16(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {bigint}
+ */
+export const decrypt32 = async (handle: bigint): Promise<bigint> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return BigInt(await getClearText(handle));
+  } else {
+    return getDecryptor().decrypt32(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {bigint}
+ */
+export const decrypt64 = async (handle: bigint): Promise<bigint> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    return BigInt(await getClearText(handle));
+  } else {
+    return getDecryptor().decrypt64(await getCiphertext(handle, ethers));
+  }
+};
+
+/**
+ * @debug
+ * This function is intended for debugging purposes only.
+ * It cannot be used in production code, since it requires the FHE private key for decryption.
+ * In production, decryption is only possible via an asyncronous on-chain call to the Gateway.
+ *
+ * @param {bigint} a handle to decrypt
+ * @returns {string}
+ */
+export const decryptAddress = async (handle: bigint): Promise<string> => {
+  if (network.name === "hardhat") {
+    await awaitCoprocessor();
+    const bigintAdd = BigInt(await getClearText(handle));
+    const handleStr = "0x" + bigintAdd.toString(16).padStart(40, "0");
+    return handleStr;
+  } else {
+    return getDecryptor().decryptAddress(await getCiphertext(handle, ethers));
+  }
+};
--- test/types.ts
+++ test/types.ts
@@ -1,5 +1,15 @@
 import type { FhevmInstance } from "fhevmjs";
 
+import type { Signers } from "./signers";
+
+declare module "mocha" {
+  export interface Context {
+    signers: Signers;
+    contractAddress: string;
+    instances: FhevmInstances;
+  }
+}
+
 export interface FhevmInstances {
   alice: FhevmInstance;
   bob: FhevmInstance;
