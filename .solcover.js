d06396e9419c82c85a29914a3e982bb173908a44
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-11-07 15:22:39 +0100

    chore: Config updates

--- .solcover.js
+++ .solcover.js
@@ -1,11 +1,9 @@
-module.exports = {
-  istanbulReporter: ["html", "lcov"],
-  providerOptions: {
-    mnemonic: process.env.MNEMONIC,
-  },
-  skipFiles: ["test", "fhevmTemp"],
-  mocha: {
-    fgrep: "[skip-on-coverage]",
-    invert: true,
-  },
+export const istanbulReporter = ["html", "lcov"];
+export const providerOptions = {
+  mnemonic: process.env.MNEMONIC,
+};
+export const skipFiles = ["mocks", "test", "fhevmTemp"];
+export const mocha = {
+  fgrep: "[skip-on-coverage]",
+  invert: true,
 };
