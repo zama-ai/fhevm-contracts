e34c76ce875eaf9d552fa73cc7f4f818adf4122b
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-11-12 11:55:43 +0100

    feat: Add EIP712 and SignatureChecker

--- contracts/test/governance/TestComp.sol
+++ contracts/test/governance/TestComp.sol
@@ -5,7 +5,12 @@ import { Comp } from "../../governance/Comp.sol";
 import { DefaultFHEVMConfig } from "../DefaultFHEVMConfig.sol";
 
 contract TestComp is DefaultFHEVMConfig, Comp {
-    constructor(address owner_) Comp(owner_) {
+    constructor(
+        address owner_,
+        string memory name_,
+        string memory symbol_,
+        string memory version_
+    ) Comp(owner_, name_, symbol_, version_) {
         //
     }
 }
