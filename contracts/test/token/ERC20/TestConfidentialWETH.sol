7432d1665b42b985ffe13482617f06edb391d53f
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-12-10 07:39:43 +0100

    fix: test contracts for Sepolia configs

--- contracts/test/token/ERC20/TestConfidentialWETH.sol
+++ contracts/test/token/ERC20/TestConfidentialWETH.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.24;
 
 import { ConfidentialWETH } from "../../../token/ERC20/ConfidentialWETH.sol";
-import { MockZamaFHEVMConfig } from "fhevm/config/ZamaFHEVMConfig.sol";
-import { MockZamaGatewayConfig } from "fhevm/config/ZamaGatewayConfig.sol";
+import { SepoliaZamaFHEVMConfig } from "fhevm/config/ZamaFHEVMConfig.sol";
+import { SepoliaZamaGatewayConfig } from "fhevm/config/ZamaGatewayConfig.sol";
 
-contract TestConfidentialWETH is MockZamaFHEVMConfig, MockZamaGatewayConfig, ConfidentialWETH {
+contract TestConfidentialWETH is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConfig, ConfidentialWETH {
     constructor(uint256 maxDecryptionDelay_) ConfidentialWETH(maxDecryptionDelay_) {}
 }
