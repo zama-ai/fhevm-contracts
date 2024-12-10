a1cbe5d858295a645037197bb12230e9a023d1e7
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-12-10 07:40:13 +0100

    test: refactor tests for fhevmjs06

--- test/confidentialERC20/ConfidentialWETH.fixture.ts
+++ test/confidentialERC20/ConfidentialWETH.fixture.ts
@@ -1,13 +1,13 @@
+import { Signer } from "ethers";
 import { ethers } from "hardhat";
 
 import type { TestConfidentialWETH } from "../../types";
-import { Signers } from "../signers";
 
-export async function deployConfidentialWETHFixture(signers: Signers): Promise<TestConfidentialWETH> {
+export async function deployConfidentialWETHFixture(account: Signer): Promise<TestConfidentialWETH> {
   // @dev We use 5 minutes for the maximum decryption delay (from the Gateway).
   const maxDecryptionDelay = 60 * 5;
   const contractFactory = await ethers.getContractFactory("TestConfidentialWETH");
-  const confidentialWETH = await contractFactory.connect(signers.alice).deploy(maxDecryptionDelay);
+  const confidentialWETH = await contractFactory.connect(account).deploy(maxDecryptionDelay);
   await confidentialWETH.waitForDeployment();
 
   return confidentialWETH;
